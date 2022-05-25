970f75474e7d0bf77c0d1d6578e887e2efdac72b
G0gg0S <g.veloudis96@gmail.com>
2022-05-25 19:03:54 +0300

    Create Server.java - Accepts a client message and prints it to console

---
+++ Project/src/main/java/com/http/Server.java
@@ -0,0 +1,21 @@
+package com.http;
+
+import java.net.*;
+import java.io.*;
+
+class Server {
+    public static void main(String args[]) throws Exception {
+        ServerSocket serverSocket = new ServerSocket(8080);
+        Socket socket = serverSocket.accept();
+        DataInputStream dataInputStream = new DataInputStream(socket.getInputStream());
+
+        String clientMessage = "";
+        while (!clientMessage.equals("stop")) {
+            clientMessage = dataInputStream.readUTF();
+            System.out.println("client says: " + clientMessage);
+        }
+        dataInputStream.close();
+        socket.close();
+        serverSocket.close();
+    }
+}
